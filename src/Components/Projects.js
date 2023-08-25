042529a78eb95350ea0cc436d4ac38d7cf5c38dc
Abdulwasim-S <abdulwasimsmech@gmail.com>
2023-08-25 10:36:34 +0530

    update

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,21 +8,45 @@ const Projects = () => {
       <div className="col-md-4 " data-aos="fade-up" data-aos-duration="500">
         <div class="card bg-dark" >
         <div className="card-img">
-          <img src="https://drive.google.com/uc?export=view&id=1v9IyfHO0QTrNOpMyQ30T4frdExTTXewo" class="card-img-top" alt="urlshort" />
+          <img src="https://drive.google.com/uc?export=view&id=1hjUVjf3T4Ut66E6X-eFYpLTrwYufrnHl" class="card-img-top" alt="urlshort" />
         </div>  
           <div class="card-body">
-            <h3 class="card-title text-warning">URL Shortener App</h3>
+            <h3 class="card-title text-warning">Job Searching App</h3>
             <p class="card-text">
-              You can shorten long url in this page for free. You just have to create a account in it.
+              You can hire and get hired by this page for free. You just have to login in it.
             </p>
             <div className="btn-row ">
-              <a href="https://url-shortener-eight-sigma.vercel.app/" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
+              <a href="https://capstone-frontend-abdulwasim-s.vercel.app/" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
                 Visit
               </a>
-              <a href="https://github.com/Abdulwasim-S/url-shortener" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
+              <a href="https://github.com/Abdulwasim-S/capstone-frontend-abdulwasim-s" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
                 Source Code FE
               </a>
-              <a href="https://github.com/Abdulwasim-S/short-url-backend" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
+              <a href="https://github.com/Abdulwasim-S/capstone-backend-abdulwasim-s" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
+                Source Code BE
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-4 " data-aos="fade-up" data-aos-duration="500">
+        <div class="card bg-dark" >
+        <div className="card-img">
+          <img src="https://drive.google.com/uc?export=view&id=1LXqQVey9gxB0esy2rQkTNVHXmtlVcq0y" class="card-img-top" alt="urlshort" />
+        </div>  
+          <div class="card-body">
+            <h3 class="card-title text-warning">Job Searching App</h3>
+            <p class="card-text">
+              You can hire and get hired by this page for free. You just have to login in it.
+            </p>
+            <div className="btn-row ">
+              <a href="https://chance-murex.vercel.app/" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
+                Visit
+              </a>
+              <a href="https://github.com/Abdulwasim-S/chance" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
+                Source Code FE
+              </a>
+              <a href="https://github.com/Abdulwasim-S/chance-backend" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
                 Source Code BE
               </a>
             </div>
@@ -71,6 +95,30 @@ const Projects = () => {
                 Source Code FE
               </a>
               <a href="..." class="btn btn-success mb-3 disabled" target="_blank" rel="noreferrer">
+                Source Code BE
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-4 " data-aos="fade-up" data-aos-duration="500">
+        <div class="card bg-dark" >
+        <div className="card-img">
+          <img src="https://drive.google.com/uc?export=view&id=1v9IyfHO0QTrNOpMyQ30T4frdExTTXewo" class="card-img-top" alt="urlshort" />
+        </div>  
+          <div class="card-body">
+            <h3 class="card-title text-warning">URL Shortener App</h3>
+            <p class="card-text">
+              You can shorten long url in this page for free. You just have to create a account in it.
+            </p>
+            <div className="btn-row ">
+              <a href="https://url-shortener-eight-sigma.vercel.app/" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
+                Visit
+              </a>
+              <a href="https://github.com/Abdulwasim-S/url-shortener" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
+                Source Code FE
+              </a>
+              <a href="https://github.com/Abdulwasim-S/short-url-backend" class="btn btn-success mb-3" target="_blank" rel="noreferrer">
                 Source Code BE
               </a>
             </div>
